c0a89e045c005b01515a38a5060da5734b0aba84
kk <kk@example.com>
2026-05-14 12:15:49 +0800

    ui重构:取消显示'预览v2组件'

--- src/screens/Home/Views/Setting/settings/Theme/IsModernUI.tsx
+++ src/screens/Home/Views/Setting/settings/Theme/IsModernUI.tsx
@@ -1,18 +1,15 @@
-import { memo, useState } from 'react'
-import { Modal, View } from 'react-native'
+import { memo } from 'react'
+import { View } from 'react-native'
 
 import CheckBoxItem from '../../components/CheckBoxItem'
 import { createStyle } from '@/utils/tools'
 import { useI18n } from '@/lang'
 import { updateSetting } from '@/core/common'
 import { useSettingValue } from '@/store/setting/hook'
-import { PillButton } from '@/components/v2/atoms'
-import { V2Showcase } from '@/components/v2/organisms/V2Showcase'
 
 export default memo(() => {
   const t = useI18n()
   const isModernUI = useSettingValue('theme.useModernUI')
-  const [previewVisible, setPreviewVisible] = useState(false)
 
   const setIsModernUI = (value: boolean) => {
     updateSetting({ 'theme.useModernUI': value })
@@ -25,26 +22,6 @@ export default memo(() => {
         label={t('setting_basic_theme_modern_ui')}
         onChange={setIsModernUI}
       />
-      {isModernUI
-        ? (
-            <View style={styles.previewWrap}>
-              <PillButton
-                label={t('setting_basic_theme_modern_ui_preview')}
-                variant="secondary"
-                size="sm"
-                onPress={() => { setPreviewVisible(true) }}
-              />
-            </View>
-          )
-        : null}
-      <Modal
-        visible={previewVisible}
-        animationType="slide"
-        presentationStyle="fullScreen"
-        onRequestClose={() => { setPreviewVisible(false) }}
-      >
-        <V2Showcase onClose={() => { setPreviewVisible(false) }} />
-      </Modal>
     </View>
   )
 })
@@ -53,9 +30,5 @@ const styles = createStyle({
   content: {
     marginTop: 5,
     marginBottom: 15,
-  },
-  previewWrap: {
-    marginTop: 8,
-    paddingLeft: 26,
   },
 })
