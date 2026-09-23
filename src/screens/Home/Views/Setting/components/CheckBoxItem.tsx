69c7c6b86c67a9f5a2640710feec441e03e3168e
echomlv <tobeok666@gmail.com>
2026-09-23 22:16:01 +0800

    feat: modernize setting toggle rows

--- src/screens/Home/Views/Setting/components/CheckBoxItem.tsx
+++ src/screens/Home/Views/Setting/components/CheckBoxItem.tsx
@@ -4,9 +4,31 @@ import { View } from 'react-native'
 
 import CheckBox, { type CheckBoxProps } from '@/components/common/CheckBox'
 import { createStyle } from '@/utils/tools'
+import { useSettingValue } from '@/store/setting/hook'
+import { SettingRow } from '@/components/v2/molecules'
 
 
 export default memo((props: CheckBoxProps) => {
+  const useModernUI = useSettingValue('theme.useModernUI')
+
+  if (useModernUI) {
+    const canToggle = !props.disabled && !(props.need && props.check)
+    return (
+      <SettingRow
+        label={props.label ?? ''}
+        description={props.helpDesc}
+        onPress={canToggle ? () => { props.onChange(!props.check) } : undefined}
+        disabled={props.disabled}
+        control={(
+          <CheckBox
+            {...props}
+            label={undefined}
+          />
+        )}
+      />
+    )
+  }
+
   return (
     <View style={styles.container}>
       <CheckBox {...props} />
@@ -21,4 +43,3 @@ const styles = createStyle({
     // marginBottom: 0,
   },
 })
-
